daeda76455a2d56bf50826a16151c9f0127efe73
msloekito <msloekito@gmail.com>
2016-05-24 23:34:22 +0000

    commit exercise up to arrays

--- arrays/exercise2.rb
+++ arrays/exercise2.rb
@@ -1,3 +1,13 @@
-arr = [["test", "hello", "world"],["example", "mem"]]
-
+#exercise 2 answer: it returns 1, and second one returns 1, 2, 3
+#exercise 3
 p arr[1][0]
+#exercise 4
+1. 3
+2. nomethoderror
+3. 8
+#exercise 5
+a = "e"
+b = "A"
+c = nil
+#exercise 6
+  # trying to modify margaret into jody, access the array using index so it would be names[3] = 'jody'
